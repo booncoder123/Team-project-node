a780b0bacbe0c1c4fc54228107bf6510be61bb10
booncoder <chanok_boon@hotmail.com>
2022-04-28 15:00:38 +0700

    fix(delete api): delete post by id

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -32,23 +32,34 @@ router.get('/get-news-post', PostService.getNewsPost);
 router.get('/get-disscusion-post', PostService.getDisscusionPost);
 router.get('/get-job-post', authMiddleware, PostService.getJobPost);
 router.get('/get-post-by-post-id', PostService.getPostByPostId);
-router.put('/like',authMiddleware, PostService.putLike);
+router.put('/like', authMiddleware, PostService.putLike);
 
-router.post("/project",authMiddleware, multer({ dest: 'temp/', fileFilter }).array('images', 10), PostService.createProject);
-router.get("/project", multer({ dest: 'temp/', fileFilter }).array('images', 10), PostService.getAllProjects);
-router.put("/project",authMiddleware, multer({ dest: 'temp/', fileFilter }).array('images', 10), PostService.updateProject);
-router.delete("/project",authMiddleware, multer({ dest: 'temp/', fileFilter }).array('images', 10), PostService.deleteProject);
-router.get("/project/projectId",  PostService.getProjectById);
-router.get("/get-disscusion-post-by-uid",authMiddleware, PostService.getDisscusionPostByUid);
-
+router.post(
+  '/project',
+  authMiddleware,
+  multer({ dest: 'temp/', fileFilter }).array('images', 10),
+  PostService.createProject,
+);
+router.get('/project', multer({ dest: 'temp/', fileFilter }).array('images', 10), PostService.getAllProjects);
+router.put(
+  '/project',
+  authMiddleware,
+  multer({ dest: 'temp/', fileFilter }).array('images', 10),
+  PostService.updateProject,
+);
+router.delete(
+  '/project',
+  authMiddleware,
+  multer({ dest: 'temp/', fileFilter }).array('images', 10),
+  PostService.deleteProject,
+);
+router.get('/project/projectId', PostService.getProjectById);
+router.get('/get-disscusion-post-by-uid', authMiddleware, PostService.getDisscusionPostByUid);
 
 //uid
-router.get("/discussion",authMiddleware, PostService.getAllDiscussion);
-router.get("/news",authMiddleware, PostService.getAllNews);
-router.get("/job",authMiddleware, PostService.getAllJob);
-router.get("/project",authMiddleware, PostService.getAllProject);
-
-
-
+router.get('/discussion', authMiddleware, PostService.getAllDiscussion);
+router.get('/news', authMiddleware, PostService.getAllNews);
+router.get('/job', authMiddleware, PostService.getAllJob);
+router.get('/project', authMiddleware, PostService.getAllProject);
 
 export default router;
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -51,9 +51,9 @@ async function deletePostByPostId(req, res, next) {
   try {
     const { postId } = req.body;
 
-    const post = await Post.findOne({ postId });
+    const post = await Post.findOne({ _id: postId });
     if (post) {
-      const deletePost = await Post.findOneAndDelete({ postId });
+      const deletePost = await Post.findOneAndDelete({ _id: postId });
       res.status(200).json({ message: 'Post deleted successfully', data: deletePost });
     } else {
       res.status(404).json({ message: 'Post not found' });
@@ -246,8 +246,8 @@ async function getPostByPostId(req, res, next) {
     const { postId } = req.body;
     console.log(postId);
 
-    const post = await Post.findOne({ _id:postId });
-    
+    const post = await Post.findOne({ _id: postId });
+
     if (post) {
       res.status(200).json({ message: 'Post fetched successfully', data: post });
     } else {
@@ -273,34 +273,34 @@ async function putLike(req, res, next) {
     const user = await User.findOne({ uid });
 
     if (user) {
-      const post = await Post.findOne({ _id :postId });
+      const post = await Post.findOne({ _id: postId });
       if (post) {
         const { likers } = post;
         console.log(likers);
-      
-        const { _id  : userId} = user;
-        
+
+        const { _id: userId } = user;
 
         if (likers.includes(userId)) {
           // const likers = post.likers.filter(liker => liker !== _id);
-          const updatePost = await Post.findOneAndUpdate({ _id:postId }, { $pull: {  likers: mongoose.Types.ObjectId(userId)  } }, { new: true });
-         
-          res.status(201).json({ message: 'Post updated successfully(unlike)', data: updatePost });
-        } 
-        else {
           const updatePost = await Post.findOneAndUpdate(
-            {  _id:postId },
+            { _id: postId },
+            { $pull: { likers: mongoose.Types.ObjectId(userId) } },
+            { new: true },
+          );
+
+          res.status(201).json({ message: 'Post updated successfully(unlike)', data: updatePost });
+        } else {
+          const updatePost = await Post.findOneAndUpdate(
+            { _id: postId },
             { $push: { likers: mongoose.Types.ObjectId(userId) } },
             { new: true },
           );
           res.status(201).json({ message: 'Post updated successfully(like)', data: updatePost });
         }
-      }
-      else{
+      } else {
         throw new Error('Post not found');
       }
-    }
-    else{
+    } else {
       throw new Error('User not found');
     }
 
@@ -321,17 +321,16 @@ async function createProject(req, res, next) {
   try {
     const { uid } = req;
     const user = await User.findOne({ uid });
-    const {_id} = user;
+    const { _id } = user;
     if (user) {
-      const { name,intro,type,year,description} = req.body;
+      const { name, intro, type, year, description } = req.body;
       const { files } = req;
-
 
       const images = await s3Service.uploadFiles(files, _id, 'project');
       const newPost = await new Post({
         images,
-        postType : "project",
-        userId : _id
+        postType: 'project',
+        userId: _id,
       });
       await newPost.save();
       console.log(newPost);
@@ -344,17 +343,13 @@ async function createProject(req, res, next) {
         projectDescription: description,
         postId: newPost._id,
       });
-      
 
       await projeectDescription.save();
-      res.status(201).json({ message: 'Project created successfully', newPost,projeectDescription });
-
-
+      res.status(201).json({ message: 'Project created successfully', newPost, projeectDescription });
     }
 
     await session.commitTransaction();
     session.endSession();
-   
   } catch (error) {
     console.log(error);
     await session.commitTransaction();
@@ -370,22 +365,24 @@ async function getAllProjects(req, res, next) {
   try {
     const posts = await Post.aggregate([
       {
-        '$match': {
-          'postType': 'project'
-        }
-      }, {
-        '$lookup': {
-          'from': 'projectdescriptions', 
-          'localField': '_id', 
-          'foreignField': 'postId', 
-          'as': 'projectDetail'
-        }
-      }, {
-        '$unwind': {
-          'path': '$projectDetail', 
-          'preserveNullAndEmptyArrays': true
-        }
-      }
+        $match: {
+          postType: 'project',
+        },
+      },
+      {
+        $lookup: {
+          from: 'projectdescriptions',
+          localField: '_id',
+          foreignField: 'postId',
+          as: 'projectDetail',
+        },
+      },
+      {
+        $unwind: {
+          path: '$projectDetail',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
     ]);
     res.status(200).json({ message: 'Posts fetched successfully', data: posts });
 
@@ -408,27 +405,18 @@ async function updateProject(req, res, next) {
     const { uid } = req;
     const user = await User.findOne({ uid });
     if (user) {
-      const { name,intro,type,year,description} = req.body;
+      const { name, intro, type, year, description } = req.body;
       const { files } = req;
-      console.log(name,intro,type,year,description,files);
+      console.log(name, intro, type, year, description, files);
       const images = await s3Service.uploadFiles(files, postId, 'posts');
-      const updatePost = await Post.findOneAndUpdate(
-        { postId },
-        { $set: { images } },
-        { new: true },
-      );
+      const updatePost = await Post.findOneAndUpdate({ postId }, { $set: { images } }, { new: true });
       const updateProject = await ProjectDescription.findOneAndUpdate(
         { postId },
-        { $set: { name,intro,type,year,description } },
+        { $set: { name, intro, type, year, description } },
         { new: true },
       );
       res.status(201).json({ message: 'Post updated successfully', data: updatePost, projectDetail: updateProject });
-
- 
-
-     
-    }
-    else{
+    } else {
       throw new Error('User not found');
     }
 
@@ -442,27 +430,29 @@ async function updateProject(req, res, next) {
   }
 }
 
-async function  deleteProject(req, res, next) {
+async function deleteProject(req, res, next) {
   const session = await Post.startSession();
   session.startTransaction();
 
   try {
     const { postId } = req.body;
     const { uid } = req;
-    const user = await User.findOne({ uid:uid });
+    const user = await User.findOne({ uid: uid });
 
-const isPostExist = await Post.findOne({ _id  : postId });
-if(isPostExist){
-  const deletedPost = await Post.findOneAndDelete({ _id:postId });
-  const deletedProject = await ProjectDescription.findOneAndDelete({ postId:postId });  
-  res.status(201).json({ message: 'Post deleted successfully', deletedPost:{
-    deletedPost,deletedProject
-  } });
-}
- 
-else{
-  throw new Error('Post not found');
-}
+    const isPostExist = await Post.findOne({ _id: postId });
+    if (isPostExist) {
+      const deletedPost = await Post.findOneAndDelete({ _id: postId });
+      const deletedProject = await ProjectDescription.findOneAndDelete({ postId: postId });
+      res.status(201).json({
+        message: 'Post deleted successfully',
+        deletedPost: {
+          deletedPost,
+          deletedProject,
+        },
+      });
+    } else {
+      throw new Error('Post not found');
+    }
     await session.commitTransaction();
     session.endSession();
   } catch (error) {
@@ -482,46 +472,52 @@ async function getProjectById(req, res, next) {
     console.log(projectId);
     const post = await Post.aggregate([
       {
-        '$match': {
-          '_id': mongoose.Types.ObjectId(projectId)
-        }
-      }, {
-        '$lookup': {
-          'from': 'projectdescriptions', 
-          'localField': '_id', 
-          'foreignField': 'postId', 
-          'as': 'project'
-        }
-      }, {
-        '$unwind': {
-          'path': '$post', 
-          'preserveNullAndEmptyArrays': true
-        }
-      }, {
-        '$lookup': {
-          'from': 'users', 
-          'localField': 'userId', 
-          'foreignField': '_id', 
-          'as': 'user'
-        }
-      }, {
-        '$unwind': {
-          'path': '$user', 
-          'preserveNullAndEmptyArrays': true
-        }
-      }, {
-        '$lookup': {
-          'from': 'comments', 
-          'localField': 'postId', 
-          'foreignField': '_id', 
-          'as': 'comments'
-        }
-      }, {
-        '$unwind': {
-          'path': '$comments', 
-          'preserveNullAndEmptyArrays': true
-        }
-      }
+        $match: {
+          _id: mongoose.Types.ObjectId(projectId),
+        },
+      },
+      {
+        $lookup: {
+          from: 'projectdescriptions',
+          localField: '_id',
+          foreignField: 'postId',
+          as: 'project',
+        },
+      },
+      {
+        $unwind: {
+          path: '$post',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'userId',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      {
+        $unwind: {
+          path: '$user',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: 'comments',
+          localField: 'postId',
+          foreignField: '_id',
+          as: 'comments',
+        },
+      },
+      {
+        $unwind: {
+          path: '$comments',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
     ]);
     res.status(200).json({ message: 'Post fetched successfully', data: post });
 
@@ -535,7 +531,7 @@ async function getProjectById(req, res, next) {
   }
 }
 
-async function getDisscusionPostByUid(req,res,next){
+async function getDisscusionPostByUid(req, res, next) {
   const session = await Post.startSession();
   session.startTransaction();
 
@@ -546,33 +542,33 @@ async function getDisscusionPostByUid(req,res,next){
       console.log(user);
       const posts = await Post.aggregate([
         {
-          '$match': {
-            'userId': user._id,
-            'postType': 'discussion'
-          }
-        }, {
-          '$lookup': {
-            'from': 'comments', 
-            'localField': '_id', 
-            'foreignField': 'postId', 
-            'as': 'comments'
-          }
-        }, {
-          '$unwind': {
-            'path': '$comments', 
-            'preserveNullAndEmptyArrays': true
-          }
-        }
+          $match: {
+            userId: user._id,
+            postType: 'discussion',
+          },
+        },
+        {
+          $lookup: {
+            from: 'comments',
+            localField: '_id',
+            foreignField: 'postId',
+            as: 'comments',
+          },
+        },
+        {
+          $unwind: {
+            path: '$comments',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
       ]);
       res.status(200).json({ message: 'Posts fetched successfully', data: posts });
 
       await session.commitTransaction();
       session.endSession();
-    }
-    else{
+    } else {
       throw new Error('User not found');
     }
-
   } catch (error) {
     console.log(error);
     await session.commitTransaction();
@@ -580,26 +576,23 @@ async function getDisscusionPostByUid(req,res,next){
     next(error);
   }
 }
-async function getAllDiscussion(req,res,next){
+async function getAllDiscussion(req, res, next) {
   const session = await Post.startSession();
   session.startTransaction();
 
   try {
-    const user = await User.findOne({ uid:req.uid });
+    const user = await User.findOne({ uid: req.uid });
     if (user) {
       const userId = user._id;
-      const posts = await Post.find({ postType: 'discussion' ,userId : userId});
+      const posts = await Post.find({ postType: 'discussion', userId: userId });
       res.status(200).json({ message: 'Posts fetched successfully', data: posts });
 
       await session.commitTransaction();
       session.endSession();
-    }
-    else{
+    } else {
       throw new Error('User not found');
     }
 
-
-    
     // res.status(200).json({ message: 'Posts fetched successfully', data: posts });
 
     await session.commitTransaction();
@@ -612,28 +605,23 @@ async function getAllDiscussion(req,res,next){
   }
 }
 
-async function getAllNews(req,res,next){
+async function getAllNews(req, res, next) {
   const session = await Post.startSession();
   session.startTransaction();
 
   try {
-    const user = await User.findOne({ uid:req.uid });
+    const user = await User.findOne({ uid: req.uid });
     if (user) {
-
       const userId = user._id;
-      const posts = await Post.find({ postType: 'news' ,userId : userId});
+      const posts = await Post.find({ postType: 'news', userId: userId });
 
-      
       res.status(200).json({ message: 'Posts fetched successfully', data: posts });
 
       await session.commitTransaction();
       session.endSession();
-    }
-    else{
+    } else {
       throw new Error('User not found');
     }
-
-
   } catch (error) {
     console.log(error);
     await session.commitTransaction();
@@ -641,25 +629,22 @@ async function getAllNews(req,res,next){
     next(error);
   }
 }
-async function getAllJob(req,res,next){
+async function getAllJob(req, res, next) {
   const session = await Post.startSession();
   session.startTransaction();
 
   try {
-    const user = await User.findOne({ uid:req.uid });
+    const user = await User.findOne({ uid: req.uid });
     if (user) {
       const userId = user._id;
-      const posts = await Post.find({ postType: 'job' ,userId : userId});
+      const posts = await Post.find({ postType: 'job', userId: userId });
       res.status(200).json({ message: 'Posts fetched successfully', data: posts });
 
       await session.commitTransaction();
       session.endSession();
-    }
-    else{
+    } else {
       throw new Error('User not found');
     }
-
-
   } catch (error) {
     console.log(error);
     await session.commitTransaction();
@@ -668,45 +653,42 @@ async function getAllJob(req,res,next){
   }
 }
 
-async function getAllProject (req, res, next) {
+async function getAllProject(req, res, next) {
   const session = await Post.startSession();
   session.startTransaction();
 
   try {
-    const user = await User.findOne({ uid:req.uid });
+    const user = await User.findOne({ uid: req.uid });
     if (user) {
       const userId = user._id;
       const posts = await Post.aggregate([
         {
-          '$lookup': {
-            'from': 'projectdescriptions', 
-            'localField': '_id', 
-            'foreignField': 'postId', 
-            'as': 'project'
-          }
-        }, {
-          '$unwind': {
-            'path': '$project', 
-            'preserveNullAndEmptyArrays': true
-          }
-        }, {
-          '$match': {
-            'userId': userId
-          }
-        }
+          $lookup: {
+            from: 'projectdescriptions',
+            localField: '_id',
+            foreignField: 'postId',
+            as: 'project',
+          },
+        },
+        {
+          $unwind: {
+            path: '$project',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $match: {
+            userId: userId,
+          },
+        },
       ]);
       res.status(200).json({ message: 'Posts fetched successfully', data: posts });
 
-
       await session.commitTransaction();
       session.endSession();
-      
-    }
-    else{
+    } else {
       throw new Error('User not found');
     }
-
-
   } catch (error) {
     console.log(error);
     await session.commitTransaction();
@@ -714,12 +696,6 @@ async function getAllProject (req, res, next) {
     next(error);
   }
 }
-
-
-
-
-
-
 
 export default {
   createPost,
@@ -740,7 +716,5 @@ export default {
   getAllDiscussion,
   getAllNews,
   getAllJob,
-  getAllProject
-
-
+  getAllProject,
 };
